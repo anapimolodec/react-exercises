d212188ce1f701934a474d312a50b01413060b06
Aiaru <aiaru.mukhamedyarova@gmail.com>
2023-07-31 17:13:10 +0900

    working on Quiz problem

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,24 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import {quizData} from './datafromAPI';
+import './Quiz.css';
 
 export default function Quiz() {
   const [data, setData] = useState(null);
   const [open, setOpen] = useState(0);
-  const [select, setSelect] = useState(null);
-  const selectAnswer = (answer) => {
-    setSelect(answer);
-  }
+
   useEffect(() => {
     setData(quizData);
   }, []);
 
 return (<>
     {data && 
-         <>
-        <Question question={data[open]} selectAnswer={selectAnswer} correct={data[open].correctAnswer}/>
+         <div className="quiz-main">
+        <Question question={data[open]} correct={data[open].correctAnswer}/>
         <button disabled={open <= 0 ? true : false} onClick={() => setOpen(open - 1)}>Back</button>
         <button disabled={open >= data.length - 1 ? true : false} onClick={() => setOpen(open + 1)}>Next</button>
-        </>
+        </div>
     }
     </>
   );
@@ -28,17 +26,18 @@ const Question = (props) => {
     const [clicked, setClicked] = useState(false);
     const [chosen, setChosen] = useState(null);
     const ans = props.question.answers;
-   
-      const updateAnswer = (i) => {
+    console.log("opened question");
+    console.log(clicked, chosen);
+    const updateAnswer = (i) => {
+        
         if (clicked === false) {
           // selectAnswer(i);
+          console.log("opened clicked question");
           setChosen(i);
-          getClicked();
+          setClicked(true);
         }
-      }
-    const getClicked = () => {
-      setClicked(true);
     }
+   
     return (
       <>
         <h1>{props.question.question}</h1>
