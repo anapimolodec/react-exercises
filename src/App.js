98ab9b9e4314d488c7f5cd32cbf49b12ed3a0b3d
Aiaru <aiaru.mukhamedyarova@gmail.com>
2023-08-02 19:07:35 +0900

    Quiz question Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-// import QuestionList from './QuestionList';
+import QuestionList from './QuestionList';
 import Quiz from './Quiz';
 
 function App() {
   return (
     <div className="App">
+      <h2 className='question-title'> Question 1: Question List</h2>
+      <QuestionList />
+      <h2 className='question-title'> Question 2: Quiz</h2>
       <Quiz />
     </div>
   );
--- src/Quiz.js
+++ src/Quiz.js
@@ -5,50 +5,56 @@ import './Quiz.css';
 export default function Quiz() {
   const [data, setData] = useState(null);
   const [open, setOpen] = useState(0);
+  const [answers, setAnswers] = useState([]);
 
   useEffect(() => {
     setData(quizData);
   }, []);
 
+  const currentlyClicked = (newAns) => {
+    let cloneAns = [...answers];
+    cloneAns[open] = newAns;
+    setAnswers(cloneAns);
+  }
+
 return (<>
     {data && 
-         <div className="quiz-main">
-        <Question question={data[open]} correct={data[open].correctAnswer}/>
-        <button disabled={open <= 0 ? true : false} onClick={() => setOpen(open - 1)}>Back</button>
-        <button disabled={open >= data.length - 1 ? true : false} onClick={() => setOpen(open + 1)}>Next</button>
-        </div>
+        <div className="quiz-main">
+          <Question question={data[open]} currentlyClicked={currentlyClicked} answer={answers[open]}/>
+          <div className='button-wrapper'>
+            <button disabled={open <= 0 ? true : false} onClick={() => setOpen(open - 1)}>Back</button>
+            <button disabled={(open >= data.length - 1 || answers[open] === undefined) ? true : false} onClick={() => setOpen(open + 1)}>Next</button>
+          </div>
+           </div>
     }
     </>
   );
 }
 
-const Question = (props) => {
-    const [clicked, setClicked] = useState(false);
-    const [chosen, setChosen] = useState(null);
-    const ans = props.question.answers;
-    console.log("opened question");
-    console.log(clicked, chosen);
-    const updateAnswer = (i) => {
-        
-        if (clicked === false) {
-          // selectAnswer(i);
-          console.log("opened clicked question");
-          setChosen(i);
-          setClicked(true);
-        }
+const Question = ({question, currentlyClicked, answer}) => {
+  const correct = question.correctAnswer;
+  const [chose, setChosen] = useState(-1);
+  useEffect(() => {
+    setChosen(-1);
+  }, [question]);
+  const handleClick = (i) => {
+    if (answer !== undefined) {
+      return ;
+    } else if (chose === -1) {
+      setChosen(i);
+      currentlyClicked(i);
     }
-   
+  }
     return (
       <>
-        <h1>{props.question.question}</h1>
-        {ans.map((option, i) => {
-         let classname ='answer ';
-        if (chosen === i) {
-          classname += props.correct === chosen ? 'correct' : 'incorrect'; 
-        }
-        return <h2 className={classname} key={i} onClick={() => updateAnswer(i)}>{option}</h2>
+        <h1>{question.question}</h1>
+        {question.answers.map((option, i) => {
+          let classname = "answer";
+          if (answer === i) {
+              i === correct ? classname += ' correct' : classname += ' incorrect';
+          }
+        return <h2 key={i} className={classname} onClick={() => handleClick(i)}>{option}</h2>
       })}
-        
       </>
     );
   }
